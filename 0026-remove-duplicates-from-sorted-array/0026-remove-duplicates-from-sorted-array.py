4305c900069481cd9199216cf2ceeeef495617c3
Tanvi Jain <tanvij.0072@gmail.com>
2023-08-13 17:36:40 +0530

    Time: 96 ms (64.88%), Space: 18 MB (13.11%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -4,27 +4,35 @@ class Solution:
         new_nums=[]
         
 #METHOD-1
-        # count=0  #maintains count of unique elements
-        # for i in range(0,n):
-        #     if(nums[i]!=nums[i-1]):
-        #         new_nums.append(nums[i])  
-        #         count+=1
-        # nums[:]=new_nums
+        count=0  #maintains count of unique elements
+
+        if(n==1):
+            return n
+
+        if(n>1):
+            new_nums = [nums[0]]
+            for i in range(1,n):
+                if(nums[i]!=nums[i-1]):
+                    new_nums.append(nums[i])  
+                    print(new_nums)
+                    count+=1
+        nums[:]=new_nums
+        #return nums
 
 
 #METHOD-2
 
-        j=0 #maintains index
-        count=0 #KEEPS COUNT OF UNIQUE ELEMENTS
+        # j=0 #maintains index
+        # count=0 #KEEPS COUNT OF UNIQUE ELEMENTS
 
-        for i in range(1,n): 
-                if (nums[i]!=nums[i-1]):
-                # new_nums.append(nums[i])
-                    nums[j+1]=nums[i]
-                    j=j+1
-                    count+=1 
-        del nums[j+1:] 
-        nums= nums[0:j]
+        # for i in range(1,n): 
+        #         if (nums[i]!=nums[i-1]):
+        #         # new_nums.append(nums[i])
+        #             nums[j+1]=nums[i]
+        #             j=j+1
+        #             count+=1 
+        # del nums[j+1:] 
+        # nums= nums[0:j]
 
        
                
