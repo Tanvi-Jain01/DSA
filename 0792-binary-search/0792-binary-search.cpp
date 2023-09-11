88c2a45438aa240aa9ec1ba9de33583b87a6b5eb
Tanvi Jain <tanvij.0072@gmail.com>
2023-09-11 21:22:07 +0530

    Time: 24 ms (87.42%), Space: 27.6 MB (10.3%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+
+       int start=0;
+       int end=nums.size()-1;
+
+        while(start<=end){
+            int mid=start+(end-start)/2; //calculate mid point
+            if (nums[mid]==target)
+            {     
+                return mid;   //return mid index
+            }          
+            else if (nums[mid]>target)  //target<mid, go left
+            {   
+                end=mid-1;     // shift the end pointer
+            }         
+            else if (nums[mid]<target) //target>mid, go right
+            {    
+                start=mid+1;     // shift the start pointer  
+            }
+        }
+        return -1;
+ 
+    }  
+    
+};
