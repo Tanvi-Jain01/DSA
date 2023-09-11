82061911d9352ead183f381707d65ea39c028c4b
Tanvi Jain <tanvij.0072@gmail.com>
2023-09-11 13:26:26 +0530

    Time: 122 ms (59.46%), Space: 16.6 MB (48.9%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.py
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.py
@@ -4,16 +4,42 @@ class Solution:
         neg=0
         n=len(nums)
 
-        if nums[-1]<0:
+        start=0
+        end=n
+
+        if nums[-1]<0 or nums[0]>0:
             return n # if last element is <0 then all are negative hence return len of List   O(1)
 
-        
-        for i in range(n):
-            if nums[i]>0:
-                pos+=1  #increment pos if >0
-            elif nums[i]<0:
-                neg+=1 #increment neg if <0
+        #SOLUTION 1
 
-        return max(pos,neg)   # O(N)
+        # for i in range(n):
+        #     if nums[i]>0:
+        #         pos+=1  #increment pos if >0
+        #     elif nums[i]<0:
+        #         neg+=1 #increment neg if <0
+
+        # return max(pos,neg)   # O(N)
         
         #we can try binary search also to make it logn
+
+#!---------------------------------------------------------------
+        #SOLUTION-2
+       
+        mid= start+(end-start)//2
+        if nums[mid]<0:
+                neg=mid-start+1
+                for i in range(mid+1,n):   #O(logn)
+                   if nums[i]<0:
+                     neg+=1
+                    
+        else:
+            for i in range(n):
+                if nums[i]>0:
+                    pos+=1  #increment pos if >0
+                elif nums[i]<0:
+                    neg+=1 #increment neg if <0    O(N)
+        
+        return max(neg, pos)
+
+          
+
