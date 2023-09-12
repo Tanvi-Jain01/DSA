77d9388bce352d4a8d05902ad6b0ae41891b1019
Tanvi Jain <tanvij.0072@gmail.com>
2023-09-12 21:42:53 +0530

    Time: 119 ms (85.38%), Space: 18.9 MB (91.03%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,36 +1,9 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        n=len(nums)
-
-        #Brute Force O(N2), but time limit exceeds.
-        # for i in range(n):
-        #     found=False
-        #     for j in range(n):
-        #         if(nums[i]==nums[j] and i!=j):
-        #            found=True
-        #            break
-        #     if not found:
-        #         return nums[i]
-
-
-
-        nums.sort()
-        # Iterate through the sorted array, comparing adjacent elements
-        for i in range(0, len(nums), 2):
-            # If the current element is not equal to the next element, it's the single element
-            if i == len(nums) - 1 or nums[i] != nums[i + 1]:
-                return nums[i]
-
-       
-
-
-
-
-
-
-
-
-              
-                
+        #XOR as 4^4=0 and 3^0=3
+        ans=0
+        for i in range(len(nums)):
+             ans=ans ^ nums[i]
+        return ans
 
         
